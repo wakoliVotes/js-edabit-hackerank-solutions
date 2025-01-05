522b3c5edf3175d8834ddc9f4da243938384a216
wakoiVotes <votesdw@yahoo.com>
2025-01-05 10:00:02 +0300

    project setup + problems

---
+++ javascript/README.md
@@ -0,0 +1,5 @@
+## List of Questions & Links to Solutions
+1.  [Solve Me First](solvemefirst.js)
+<img src='images/problem0.png' >
+2. [Arrays - DS](array-ds.js)
+<img src='images/problem1.png' >
---
+++ javascript/array-ds.js
@@ -0,0 +1,8 @@
+/*
+An array is a type of data structure that stores elements of the same type in a contiguous block of memory. In an array, A. of size N. each memory location has some unique index, i (where 0<i<N), that can be referenced as Ali) or A. Reverse an array of integers. Note: If you've already solved our C++ domain's Arrays Introduction challenge, you may want to skip this. Example A = [1,2,3] Return [3, 2, 1]. Function Description Complete the function reverseArray in the editor below. reverseArray has the following parameter(s): int Aln]: the array to reverse Returns int[n]: the reversed array Input Format The first line contains an integer, N, the number of integers in A. The second line contains N space-separated integers that make up A. Constraints 1 ≤ N ≤ 103 1A[i] < 104, where Ali] is the ith integer in A
+
+
+
+*/
+
+
