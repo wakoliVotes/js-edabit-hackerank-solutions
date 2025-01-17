4584c4807f8796830022d66c7722013082e00318
wakoiVotes <votesdw@yahoo.com>
2025-01-17 09:04:36 +0300

    kotlin

---
+++ kotlin/solvemefirst.kt
@@ -0,0 +1,30 @@
+/*
+ Complete the function solveMeFirst to compute the sum of two integers.
+
+Example
+a = 7
+b = 3
+
+Return .
+
+Function Description
+
+Complete the solveMeFirst function in the editor below.
+
+solveMeFirst has the following parameters:
+
+int a: the first value
+int b: the second value
+Returns
+- int: the sum of  and 
+
+Constraints
+1 ≤ a, b ≤ 1000
+
+*/
+
+// Original question here: https://www.hackerrank.com/challenges/solve-me-first/problem
+
+fun solveMeFirst (arr) {
+
+}
